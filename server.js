f4be9cd8064fbb14cf488949b558881145f6d1f3
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-01-31 08:03:05 -0800

    Added wildcard routing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -11,6 +12,10 @@ app.use(notesController);
 
 const apiController = require("./controllers/apiController");
 app.use(apiController);
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
 
 app.listen(3001, function() {
     console.log("Server Started on Port 3001");
